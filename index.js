719ab7a951badd718e8d00fe643034bcdb1bef81
awaisjameel4958 <awais@nbthub.com>
2024-06-27 22:08:24 +0500

    Local test made first.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import { getThumbnailWithPath, getThumbnailWithUrl } from "./utils.js";
 
 (async () => {
-    console.log("1 test start");
-    const thumbnailPathUrl = await getThumbnailWithUrl('https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4');
-    console.log("thumbnailPathUrl :", thumbnailPathUrl);
-    console.log("1 test end");
+    console.log("test start");
+    const thumbnailPathLocal = await getThumbnailWithPath('./test-video/test-001.mp4');
+    console.log("thumbnailPathLocal :", thumbnailPathLocal);
+    console.log("test end");
 
     console.log("--------------------");
 
-    console.log("2 test start");
-    const thumbnailPathLocal = await getThumbnailWithPath('./test-video/test-001.mp4');
-    console.log("thumbnailPathLocal :", thumbnailPathLocal);
-    console.log("2 test end");
+    console.log("test start");
+    const thumbnailPathUrl = await getThumbnailWithUrl('https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4');
+    console.log("thumbnailPathUrl :", thumbnailPathUrl);
+    console.log("test end");
 })()
