6b397cc9dfd89d022213f77c0a44d22d6cba5856
awaisjameel4958 <awais@nbthub.com>
2024-06-27 21:40:14 +0500

    Video thumbnail generation feature added.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-import { generateThumbnail } from "./utils.js";
+import { getThumbnailWithPath, getThumbnailWithUrl } from "./utils.js";
 
 (async () => {
-    const thumbnailPath = await generateThumbnail('https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4');
-    console.log(thumbnailPath);
+    console.log("1 test start");
+    const thumbnailPathUrl = await getThumbnailWithUrl('https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4');
+    console.log("thumbnailPathUrl :", thumbnailPathUrl);
+    console.log("1 test end");
+
+    console.log("--------------------");
+
+    console.log("2 test start");
+    const thumbnailPathLocal = await getThumbnailWithPath('./test-video/test-001.mp4');
+    console.log("thumbnailPathLocal :", thumbnailPathLocal);
+    console.log("2 test end");
 })()
--- utils.js
+++ utils.js
@@ -1,62 +1,105 @@
 import axios from 'axios';
+import ffmpeg from 'fluent-ffmpeg';
+import { path as ffmpegPath } from '@ffmpeg-installer/ffmpeg';
+
 import fs from 'fs';
 import path from 'path';
-import url from 'url';
 import { promisify } from 'util';
 
 const writeFileAsync = promisify(fs.writeFile);
 const unlinkAsync = promisify(fs.unlink);
 
-const tempDir = path.join(process.cwd(), 'temp');
-const tempDownloadDir = path.join(tempDir, 'downloads');
-
+const thumbsDir = 'thumbnails';
+const currentDirPath = process.cwd();
+const tempDirPath = path.join(currentDirPath, 'temp');
+const tempDownloadDirPath = path.join(tempDirPath, 'downloads');
+const thumbsDirPath = path.join(currentDirPath, thumbsDir);
 
 const getFileNameFromLocalPath = (localPath) => {
     const fileName = path.basename(localPath);
     return fileName;
 }
 const getFileNameFromURL = (fileUrl) => {
-    const parsedUrl = new url(fileUrl);
+    const parsedUrl = new URL(fileUrl);
     const fileName = path.basename(parsedUrl.pathname);
     return fileName;
 }
 
-export async function getVideoThumbnail(videoLocalPath) {
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+export async function generateThumbnail(videoLocalPath) {
     try {
         const videoFileName = getFileNameFromLocalPath(videoLocalPath).split('.')[0];
-        console.log("received video local path: ", videoLocalPath);
+        const ext = getFileNameFromLocalPath(videoLocalPath).split('.')[1];
+        const thumbnailFilename = `${videoFileName}_thumbnail.png`;
+        const thumbnailFileUrl = `/${thumbsDir}/${thumbnailFilename}`;
+        console.log("generateThumbnail | received video local path: ", videoLocalPath);
+
+        return new Promise((resolve, reject) => {
+            ffmpeg(videoLocalPath)
+                .inputFormat(ext)
+                .on('filenames', function (filenames) {
+                    console.log('generateThumbnail | Will generate thumbnails: ', filenames);
+                })
+                .on('end', async function () {
+                    console.log('generateThumbnail | Generated thumbnail successfully at: ', thumbnailFileUrl);
+
+                    resolve(thumbnailFileUrl);
+                })
+                .on('error', async function (err) {
+                    console.error('generateThumbnail | Error generating thumbnail: ', err.stack);
+                    reject(err);
+                })
+                .screenshots({
+                    // count: 1,
+                    timemarks: ['0.1'], // Capture at 0.1 seconds
+                    // size: '360x640',
+                    folder: thumbsDirPath,
+                    filename: thumbnailFilename,
+                });
+        });
+    } catch (err) {
+        console.error('generateThumbnail | Error: ', err.stack);
+        throw err;
+    }
+}
+
+export async function getThumbnailWithPath(videoLocalPath) {
+    try {
+        console.log("getThumbnailWithPath | received video local path: ", videoLocalPath);
 
         return await new Promise(async (resolve, reject) => {
             try {
-                resolve(`thumbnail file path: {} for video : ${videoFileName}`);
+                resolve(await generateThumbnail(videoLocalPath));
             } catch (err) {
                 reject(err);
             }
         })
     } catch (err) {
-        console.error('Error getVideoThumbnail:', err.stack);
+        console.error('getThumbnailWithPath | Error: ', err.stack);
         throw err;
     }
 }
 
-export async function generateThumbnail(videoUrl) {
+export async function getThumbnailWithUrl(videoUrl) {
     try {
-        console.log("received video url: ", videoUrl);
+        console.log("getThumbnailWithUrl | received video url: ", videoUrl);
 
         const response = await axios.get(videoUrl, { responseType: 'arraybuffer' });
 
+        const videoFileName = getFileNameFromURL(videoUrl).split('.')[0];
         const ext = getFileNameFromURL(videoUrl).split('.').pop().toLowerCase();
-        const tempVideoPath = path.join(tempDownloadDir, `/temp_video.${ext}`);
+        const tempVideoPath = path.join(tempDownloadDirPath, `/${videoFileName}.${ext}`);
 
-        if (!fs.existsSync(tempDownloadDir)) {
-            fs.mkdirSync(tempDownloadDir, { recursive: true });
+        if (!fs.existsSync(tempDownloadDirPath)) {
+            fs.mkdirSync(tempDownloadDirPath, { recursive: true });
         }
 
         await writeFileAsync(tempVideoPath, response.data);
 
         return await new Promise(async (resolve, reject) => {
             try {
-                const thumbnailPath = await getVideoThumbnail(tempVideoPath)
+                const thumbnailPath = await getThumbnailWithPath(tempVideoPath)
                 await unlinkAsync(tempVideoPath);
 
                 resolve(thumbnailPath);
@@ -65,7 +108,7 @@ export async function generateThumbnail(videoUrl) {
             }
         })
     } catch (err) {
-        console.error('Error generateThumbnail:', err.stack);
+        console.error('getThumbnailWithUrl | Error: ', err.stack);
         throw err;
     }
 
